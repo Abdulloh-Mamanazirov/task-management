1ca739eb3198fd6d816ee9bfa276e84eb1881016
Abdulloh <abdullajonmamnazirov@gmail.com>
2023-12-09 16:18:07 +0500

    add: voice recorder

--- src/pages/commonPages/CreateTask/index.jsx
+++ src/pages/commonPages/CreateTask/index.jsx
@@ -2,31 +2,30 @@ import { useRef } from "react";
 import { AudioRecorder } from "react-audio-voice-recorder";
 
 const CreateTaskindex = () => {
-  const audioList = useRef()
-  
+  const audioList = useRef();
+
   const addAudioElement = (blob) => {
+    console.log(blob);
     const url = URL.createObjectURL(blob);
     const audio = document.createElement("audio");
     audio.src = url;
     audio.controls = true;
-    document.body.appendChild(audio);
+    audioList.current.appendChild(audio);
   };
-  
+
   return (
     <div>
       <div>
-      <AudioRecorder
-        onRecordingComplete={addAudioElement}
-        audioTrackConstraints={{
-          noiseSuppression: true,
-          echoCancellation: true,
-        }}
-        downloadOnSavePress={true}
-        downloadFileExtension="mp3"
-      />
-      <ul ref={audioList}>
-        xx
-      </ul>
+        <AudioRecorder
+          onRecordingComplete={addAudioElement}
+          audioTrackConstraints={{
+            noiseSuppression: true,
+            echoCancellation: true,
+          }}
+          downloadOnSavePress={true}
+          downloadFileExtension="mp3"
+        />
+        <ul ref={audioList}></ul>
       </div>
     </div>
   );
