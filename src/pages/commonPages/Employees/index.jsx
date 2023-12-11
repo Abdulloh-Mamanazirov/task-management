7157aa8d370e26b5e450a861b060eb82d9a3b4e1
Abdulloh <abdullajonmamnazirov@gmail.com>
2023-12-11 11:53:13 +0500

    add: optional chaining

--- src/pages/commonPages/Employees/index.jsx
+++ src/pages/commonPages/Employees/index.jsx
@@ -106,7 +106,7 @@ const index = () => {
                         <Chip
                           label={
                             user?.sector
-                              ? sectors.map(
+                              ? sectors?.map?.(
                                   (i) => i.id === user?.sector && i.name
                                 )
                               : "Tayinlanmagan"
