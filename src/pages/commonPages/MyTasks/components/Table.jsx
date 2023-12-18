af5ebcfb0b33745d06ea79764ffb800de110252c
xakimovotabek200 <xakimovoatbek@gmail.com>
2023-12-18 13:49:38 +0500

    add edit table page

--- src/pages/commonPages/MyTasks/components/Table.jsx
+++ src/pages/commonPages/MyTasks/components/Table.jsx
@@ -1,19 +1,4 @@
-import {
-    Chip,
-    Paper,
-    Table,
-    Avatar,
-    Button,
-    Dialog,
-    TableRow,
-    TableCell,
-    TableBody,
-    TableHead,
-    TableFooter,
-    AvatarGroup,
-    TableContainer,
-    TablePagination,
-} from "@mui/material";
+import { Avatar, AvatarGroup, Chip, Dialog } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
@@ -24,36 +9,40 @@ const TableTasks = () => {
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(10);
 
+    const user_id = sessionStorage.getItem("user_id")
+    console.log(user_id);
     async function getData() {
-        const response = await axios.get("bolim/");
+        const response = await axios.get(`/task/${user_id}/`);
         setData(response?.data);
     }
-
-    const handleChangePage = (_, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
+    console.log(data, "task");
 
     useEffect(() => {
         getData();
     }, []);
 
+    const getStatusColor = (status) => {
+        if (status === "bajarildi") {
+            return "green-500";
+        } else if (status === "Bajarilmoqda") {
+            return "#D6DB00";
+        } else {
+            return "red";
+        }
+    };
+
     return (
         <>
-            <div className="grid md:grid-cols-3 gap-10 justify-between mt-5">
-                <div className="md:w-full bg-white rounded-[5px] border border-zinc-800 p-5  shadow-md">
+            <div className="grid md:grid-cols-3 justify-between mt-5">
+                <div className="w-[500px] bg-white rounded-[5px] border border-zinc-800 p-5  shadow-md">
                     <div className="flex items-center justify-around gap-10">
                         <div>
                             <p className="text-black font-normal">
-                                some text here and <br /> here and here
+                                {data?.text?.length > 0 ? data?.text : "Some Text and nimadur"}{" "}
                             </p>
                             <div className="mt-4 rounded-md">
                                 <audio controls className="w-[250px]">
-                                    <source src=" https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3" />
+                                    <source src={data?.audio} />
                                 </audio>
                             </div>
                             <div className="mt-4">
@@ -78,10 +67,8 @@ const TableTasks = () => {
                                     21-12-2024
                                 </div>
                             </div>
-                            <div className="mt-5 ml-5 ">
-                                <div className="text-black  font-medium ">
-                                    Tugash sanasi:
-                                </div>
+                            <div className="mt-5 ">
+                                <div className="text-black  font-medium ">Tugash sanasi:</div>
                                 <div className="text-black text-center  font-normal ">
                                     21-12-2024
                                 </div>
@@ -91,163 +78,38 @@ const TableTasks = () => {
                                     Status:
                                 </div>
                                 <div className="text-black font-normal flex items-center justify-between ">
-                                    <Chip
-                                        label={
-                                            data?.sector
-                                                ? sectors?.map?.(
-                                                    (i) => i.id === data?.sector && i.name
-                                                )
-                                                : "tayinlanmadi"
-                                        }
-                                        color={data?.sector ? "primary" : "error"}
-                                        variant="outlined"
-                                    />
+                                    {data?.sector ? (
+                                        <div
+                                            style={{
+                                                backgroundColor: getStatusColor(data.sector),
+                                                padding: "5px 10px",
+                                                borderRadius: "5px",
+                                            }}
+                                        >
+                                            {sectors?.find((i) => i.id === data.sector)?.name ||
+                                                "tayinlanmadi"}
+                                        </div>
+                                    ) : (
+                                        <div
+                                            className="text-white"
+                                            style={{
+                                                backgroundColor: getStatusColor("default"),
+                                                padding: "5px 10px",
+                                                borderRadius: "5px",
+                                            }}
+                                        >
+                                            tayinlanmadi
+                                        </div>
+                                    )}
                                     <div>
                                         <span className="fa-solid fa-edit text-xl text-blue-500 cursor-pointer" />
                                     </div>
-
                                 </div>
                             </div>
                         </div>
-
-                    </div>
-                </div>
-                <div className="w-full  bg-white rounded-[5px] border border-zinc-800 p-5">
-                    <div className="flex items-center justify-around gap-10">
-                        <div>
-                            <p className="text-black font-normal">
-                                some text here and <br /> here and here
-                            </p>
-                            <div className="mt-4 rounded-md">
-                                <audio controls className="w-[250px]">
-                                    <source src=" https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3" />
-                                </audio>
-                            </div>
-                            <div className="mt-4">
-                                <AvatarGroup
-                                    onClick={() => setModal({ open: true, data: "modal data" })}
-                                    className="w-fit"
-                                    max={4}
-                                    style={{ cursor: "pointer" }}
-                                >
-                                    {new Array(5).fill(null).map((_, ind) => (
-                                        <Avatar alt={`${ind}`} src="..." />
-                                    ))}
-                                </AvatarGroup>
-                            </div>
-                        </div>
-                        <div>
-                            <div className=" ">
-                                <div className="text-black  font-medium ">
-                                    Boshlanish sanasi:
-                                </div>
-                                <div className="text-black text-center  font-normal ">
-                                    21-12-2024
-                                </div>
-                            </div>
-                            <div className="mt-5 ml-5">
-                                <div className="text-black font-medium ">
-                                    Tugash sanasi:
-                                </div>
-                                <div className="text-black text-center  font-normal ">
-                                    21-12-2024
-                                </div>
-                            </div>
-                            <div className="mt-5 ">
-                                <div className="text-black text-center font-medium ">
-                                    Status:
-                                </div>
-                                <div className="text-black font-normal flex items-center justify-between ">
-                                    <Chip
-                                        label={
-                                            data?.sector
-                                                ? sectors?.map?.(
-                                                    (i) => i.id === data?.sector && i.name
-                                                )
-                                                : "tayinlanmadi"
-                                        }
-                                        color={data?.sector ? "primary" : "error"}
-                                        variant="outlined"
-                                    />
-                                    <div>
-                                        <span className="fa-solid fa-edit text-xl text-blue-500 cursor-pointer" />
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div className="w-full  bg-white rounded-[5px] border border-zinc-800 p-5">
-                    <div className="flex items-center justify-around gap-10">
-                        <div>
-                            <p className="text-black font-normal">
-                                some text here and <br /> here and here
-                            </p>
-                            <div className="mt-4 rounded-md">
-                                <audio controls className="w-[250px]">
-                                    <source src=" https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3" />
-                                </audio>
-                            </div>
-                            <div className="mt-4">
-                                <AvatarGroup
-                                    onClick={() => setModal({ open: true, data: "modal data" })}
-                                    className="w-fit"
-                                    max={4}
-                                    style={{ cursor: "pointer" }}
-                                >
-                                    {new Array(5).fill(null).map((_, ind) => (
-                                        <Avatar alt={`${ind}`} src="..." />
-                                    ))}
-                                </AvatarGroup>
-                            </div>
-                        </div>
-                        <div>
-                            <div className=" ">
-                                <div className="text-black  font-medium ">
-                                    Boshlanish sanasi:
-                                </div>
-                                <div className="text-black text-center  font-normal ">
-                                    21-12-2024
-                                </div>
-                            </div>
-                            <div className="mt-5 ml-5">
-                                <div className="text-black  font-medium ">
-                                    Tugash sanasi:
-                                </div>
-                                <div className="text-black text-center  font-normal ">
-                                    21-12-2024
-                                </div>
-                            </div>
-                            <div className="mt-5 ">
-                                <div className="text-black text-center font-medium ">
-                                    Status:
-                                </div>
-                                <div className="text-black font-normal flex items-center justify-between ">
-                                    <Chip
-                                        label={
-                                            data?.sector
-                                                ? sectors?.map?.(
-                                                    (i) => i.id === data?.sector && i.name
-                                                )
-                                                : "tayinlanmadi"
-                                        }
-                                        color={data?.sector ? "primary" : "error"}
-                                        variant="outlined"
-                                    />
-                                    <div>
-                                        <span className="fa-solid fa-edit text-xl text-blue-500 cursor-pointer" />
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-
                     </div>
                 </div>
             </div>
-
             <Dialog
                 open={modal.open}
                 onClose={() => setModal({ open: false, data: null })}
@@ -256,48 +118,14 @@ const TableTasks = () => {
                 aria-describedby="edit-modal"
             >
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 px-5 mt-5 mb-5 rounded-md">
-                    <div className="cursor-pointer">
-                        <img
-                            src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"
-                            className="rounded-xl"
-                            alt=""
-                        />
-                    </div>
-                    <div className="cursor-pointer">
-                        <img
-                            src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"
-                            className="rounded-xl"
-                            alt=""
-                        />
-                    </div>
-                    <div className="cursor-pointer">
-                        <img
-                            src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"
-                            className="rounded-xl"
-                            alt=""
-                        />
-                    </div>
-                    <div className="cursor-pointer">
-                        <img
-                            src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"
-                            className="rounded-xl"
-                            alt=""
-                        />
-                    </div>
-                </div>
-            </Dialog>
-
-            <Dialog
-                open={modalPlay.open}
-                onClose={() => setModalPlay({ open: false, data: null })}
-                fullWidth
-                keepMounted
-                aria-describedby="edit-modal"
-            >
-                <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 px-5 mt-5 mb-5 rounded-md">
-                    <audio controls>
-                        <source src=" https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3" />
-                    </audio>
+                    {data?.photo?.map((item) => {
+                        return (
+                            <div className="cursor-pointer" key={item.id}>
+                                {console.log(item.photo.photo)}
+                                <img src={item.photo} className="rounded-xl" alt="" />
+                            </div>
+                        );
+                    })}
                 </div>
             </Dialog>
         </>
