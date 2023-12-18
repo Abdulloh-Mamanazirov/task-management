df254971e28e999d092f34268642fbc537e070bb
Abdulloh <abdullajonmamnazirov@gmail.com>
2023-12-18 14:43:15 +0500

    add: new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     extend: {
       colors: {
         primary: "#1976d2",
-        green: "#4caf50",
+        "custom-green": "#14FF00",
+        "custom-red": "#FF0000",
+        "custom-yellow": "#FFF500",
+        "custom-light-green": "#04DB004F",
+        "custom-light-red": "#DB00004F",
+        "custom-light-yellow": "#D7DB004F",
       },
       screens: {
         "md-lg": "900px",
