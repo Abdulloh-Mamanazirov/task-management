a6c5ef4e394e17b35c062857d4e42c278ca23083
Abdulloh <abdullajonmamnazirov@gmail.com>
2023-12-18 17:44:55 +0500

    fix: tasks & add: loading

--- src/pages/commonPages/MyTasks/components/Tasks.jsx
+++ src/pages/commonPages/MyTasks/components/Tasks.jsx
@@ -1,9 +1,9 @@
-import { Avatar, AvatarGroup, Dialog } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Avatar, AvatarGroup, Dialog } from "@mui/material";
 
 const Tasks = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
   const [modal, setModal] = useState({ open: false, data: null });
   const user_id = sessionStorage.getItem("user_id");
 
@@ -15,8 +15,6 @@ const Tasks = () => {
   useEffect(() => {
     getData();
   }, []);
-
-  if (!data) return <h1>loading...</h1>;
 
   const getStatus = (status) => {
     if (status === "bajarildi") {
@@ -43,79 +41,122 @@ const Tasks = () => {
   return (
     <>
       <div className="w-full flex items-center flex-wrap gap-5 mt-5">
-        {Array.isArray(data) &&
-          data.map((item) => {
-            return (
-              <div
-                key={item.id}
-                className="bg-white rounded-lg border border-gray-400 p-2  shadow-md flex items-start flex-col sm:flex-row gap-3 sm:gap-0 w-full sm:w-auto"
-              >
-                <div>
-                  <p className="text-black font-normal text-lg max-w-xs">
-                    {item?.text?.[0]?.text.length > 0
-                      ? item?.text?.[0]?.text
-                      : "Some Text and nimadur"}{" "}
-                  </p>
-                  <div className="mt-4 rounded-md">
-                    {!item?.audio?.[0]?.audio.includes('null') && (
-                      <audio controls className="sm:w-[250px]">
-                        <source
-                          src={
-                            `https://xodim.pythonanywhere.com/` +
-                            item?.audio?.[0]?.audio
-                          }
-                        />
-                      </audio>
-                    )}
-                    {item?.audio?.[0]?.audio === null && (
-                      <p>No audio available</p>
-                    )}
-                  </div>
-                  <div className="mt-4">
-                    <AvatarGroup
-                      onClick={() =>
-                        setModal({ open: true, data: item.photo })
-                      }
-                      className="w-fit"
-                      max={4}
-                      style={{ cursor: "pointer" }}
-                    >
-                      {Array.isArray(item?.photo) &&
-                        item?.photo?.map((item) => (
-                          <Avatar
-                            alt="salom"
-                            style={{ border: "1px solid black" }}
-                            key={item.id}
+        {Array.isArray(data)
+          ? data.map((item) => {
+              return (
+                <div
+                  key={item?.id}
+                  className="bg-white rounded-lg border border-gray-400 p-2  shadow-md flex items-start flex-col sm:flex-row gap-3 sm:gap-0 w-full sm:min-w-[404px] min-h-[221px] sm:w-auto"
+                >
+                  <div>
+                    <p className="text-black font-normal text-lg max-w-xs">
+                      {item?.text?.[0]?.text.length > 0
+                        ? item?.text?.[0]?.text
+                        : null}{" "}
+                    </p>
+                    <div className="mt-4 rounded-md">
+                      <p className="text-left font-medium ">Audio:</p>
+                      {!item?.audio?.[0]?.audio.includes("null") ? (
+                        <audio controls className="sm:w-[250px]">
+                          <source
                             src={
-                              `https://xodim.pythonanywhere.com/` + item?.photo
+                              `https://xodim.pythonanywhere.com/` +
+                              item?.audio?.[0]?.audio
                             }
                           />
-                        ))}
-                    </AvatarGroup>
+                        </audio>
+                      ) : (
+                        <em className="text-sm">Audio mavjud emas</em>
+                      )}
+                    </div>
+                    <div className="mt-4">
+                      <p className="text-left font-medium ">Rasmlar:</p>
+                      <AvatarGroup
+                        onClick={() =>
+                          setModal({ open: true, data: item.photo })
+                        }
+                        className="w-fit"
+                        max={4}
+                        style={{ cursor: "pointer" }}
+                      >
+                        {item?.photo?.length > 0 ? (
+                          item?.photo?.map((item) => (
+                            <Avatar
+                              alt="Image"
+                              style={{ border: "1px solid black" }}
+                              key={item.id}
+                              src={
+                                `https://xodim.pythonanywhere.com/` +
+                                item?.photo
+                              }
+                            />
+                          ))
+                        ) : (
+                          <em className="text-sm">Rasm mavjud emas</em>
+                        )}
+                      </AvatarGroup>
+                    </div>
                   </div>
-                </div>
-                <div className="w-full">
-                  <div>
-                    <p className="text-right font-medium ">
-                      Boshlanish sanasi:
-                    </p>
-                    <p className="text-right font-normal ">21-12-2024</p>
-                  </div>
-                  <div className="mt-5">
-                    <p className="text-right font-medium">Tugash sanasi:</p>
-                    <p className="text-right font-normal">{item?.deadline}</p>
-                  </div>
-                  <div className="mt-5 ">
-                    <p className="text-right font-medium ">Status:</p>
-                    <div className="font-normal flex gap-2 items-center justify-end">
-                      {getStatus(item?.status)}
-                      <span className="fa-solid fa-edit text-xl text-blue-500 cursor-pointer" />
+                  <div className="w-full">
+                    <div>
+                      <p className="text-right font-medium ">
+                        Boshlanish sanasi:
+                      </p>
+                      <p className="text-right font-normal ">21-12-2024</p>
+                    </div>
+                    <div className="mt-5">
+                      <p className="text-right font-medium">Tugash sanasi:</p>
+                      <p className="text-right font-normal">{item?.deadline}</p>
+                    </div>
+                    <div className="mt-5 ">
+                      <p className="text-right font-medium ">Status:</p>
+                      <div className="font-normal flex gap-2 items-center justify-end">
+                        {getStatus(item?.status)}
+                        <span className="fa-solid fa-edit text-xl text-blue-500 cursor-pointer" />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })
+          : new Array(2).fill(null).map((_, ind) => {
+              return (
+                <div
+                  key={ind}
+                  className="bg-white rounded-lg border border-gray-400 p-2  shadow-md flex items-start flex-col sm:flex-row gap-3 sm:gap-0 w-full sm:min-w-[404px] min-h-[221px] sm:w-auto"
+                >
+                  <div>
+                    <p className="text-black font-normal text-lg w-full p-3 bg-gray-200" />
+                    <div className="mt-4 rounded-md">
+                      <p className="text-black font-normal text-lg w-[200px] p-3 bg-gray-200" />
+                    </div>
+                    <div className="mt-4">
+                      <p className="text-left font-medium ">Rasmlar:</p>
+                      <AvatarGroup
+                        className="w-fit"
+                        max={4}
+                        style={{ cursor: "pointer" }}
+                      >
+                        {new Array(3).fill(null).map((_, ind) => (
+                          <Avatar alt={ind + 1} key={ind} src={"..."} />
+                        ))}
+                      </AvatarGroup>
+                    </div>
+                  </div>
+                  <div className="w-full">
+                    <div>
+                      <p className="text-black font-normal w-1/2 ml-auto p-3 bg-gray-200" />
+                    </div>
+                    <div className="mt-5">
+                      <p className="text-black font-normal w-1/2 ml-auto p-3 bg-gray-200" />
+                    </div>
+                    <div className="mt-5 ">
+                      <p className="text-black font-normal w-1/2 ml-auto p-3 bg-gray-200" />
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
       </div>
       <Dialog
         open={modal.open}
@@ -131,9 +172,7 @@ const Tasks = () => {
                 <img
                   alt="salom"
                   key={item.id}
-                  src={
-                    `https://xodim.pythonanywhere.com/` + item?.photo
-                  }
+                  src={`https://xodim.pythonanywhere.com/` + item?.photo}
                 />
               </div>
             );
