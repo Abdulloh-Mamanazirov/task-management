eea4a95a11571ad4966a344f9ddd9db23a9af6bd
Abdulloh <abdullajonmamnazirov@gmail.com>
2023-12-27 14:12:32 +0500

    fix manager getting tasks

--- src/pages/commonPages/MyTasks/index.jsx
+++ src/pages/commonPages/MyTasks/index.jsx
@@ -5,11 +5,17 @@ import { Tasks, Calendar, MyTaskTable } from "./components";
 const index = () => {
   const [data, setData] = useState();
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const status = sessionStorage.getItem("status");
   const user_id = sessionStorage.getItem("user_id");
 
   async function getData() {
-    const response = await axios.get(`/task/${user_id}/`);
-    setData(response?.data);
+    if (status === "manager") {
+      const response = await axios.get(`/task/one/manager/${user_id}/`);
+      setData(response?.data);
+    } else {
+      const response = await axios.get(`/task/${user_id}/`);
+      setData(response?.data);
+    }
   }
 
   useEffect(() => {
