1a83ff19f27c89dc9198a5272b102232d893c1b3
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-01-06 13:13:29 +0500

    add notes page header

--- src/pages/commonPages/Notes/App.jsx
+++ src/pages/commonPages/Notes/App.jsx
@@ -5,6 +5,7 @@ import Sidebar from "./components/Sidebar";
 import Month from "./components/Month";
 import GlobalContext from "./context/GlobalContext";
 import EventModal from "./components/EventModal";
+import CalendarHeader from "./components/CalendarHeader";
 function App() {
   const [currenMonth, setCurrentMonth] = useState(getMonth());
   const { monthIndex, showEventModal } = useContext(GlobalContext);
@@ -17,7 +18,8 @@ function App() {
     <React.Fragment>
       {showEventModal && <EventModal />}
 
-      <div className="h-screen flex flex-col">
+      <div className="h-[calc(100vh-80px)] flex flex-col">
+        <CalendarHeader />
         <div className="flex flex-1">
           <Sidebar />
           <Month month={currenMonth} />
---
+++ src/pages/commonPages/Notes/components/CalendarHeader.jsx
@@ -0,0 +1,41 @@
+import dayjs from "dayjs";
+import React, { useContext } from "react";
+import GlobalContext from "../context/GlobalContext";
+export default function CalendarHeader() {
+  const { monthIndex, setMonthIndex } = useContext(GlobalContext);
+  function handlePrevMonth() {
+    setMonthIndex(monthIndex - 1);
+  }
+  function handleNextMonth() {
+    setMonthIndex(monthIndex + 1);
+  }
+  function handleReset() {
+    setMonthIndex(
+      monthIndex === dayjs().month()
+        ? monthIndex + Math.random()
+        : dayjs().month()
+    );
+  }
+  return (
+    <header className="px-4 py-2 flex items-center justify-center">
+      <button onClick={handleReset} className="border rounded py-2 px-4 mr-5">
+        Bugunga qaytish
+      </button>
+      <button
+        onClick={handlePrevMonth}
+        className="w-8 aspect-square inline-grid place-items-center rounded-full border"
+      >
+        <span className="fa-solid fa-chevron-left" />
+      </button>
+      <h2 className="mx-4 text-xl text-gray-500 font-bold">
+        {dayjs(new Date(dayjs().year(), monthIndex)).format("MMMM YYYY")}
+      </h2>
+      <button
+        onClick={handleNextMonth}
+        className="w-8 aspect-square inline-grid place-items-center rounded-full border"
+      >
+        <span className="fa-solid fa-chevron-right" />
+      </button>
+    </header>
+  );
+}
